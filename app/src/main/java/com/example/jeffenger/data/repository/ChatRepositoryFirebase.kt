3cc76762f69a4d660f7475793fd6464c0f44661c
Jeff Braun <braun_jeff@web.de>
2026-02-13 09:55:45 +0100

    - Added new fun observeChatsForUser to (until now) unused ChatRepositoryFirebase.kt

--- app/src/main/java/com/example/jeffenger/data/repository/ChatRepositoryFirebase.kt
+++ app/src/main/java/com/example/jeffenger/data/repository/ChatRepositoryFirebase.kt
@@ -1,13 +1,20 @@
 package com.example.jeffenger.data.repository
 
+import com.example.jeffenger.data.local.MockData.chats
 import com.example.jeffenger.data.remote.model.Chat
 import com.example.jeffenger.data.remote.model.Message
 import com.example.jeffenger.data.remote.model.User
 import com.example.jeffenger.data.repository.interfaces.ChatRepositoryInterface
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.map
 
 class ChatRepositoryFirebase : ChatRepositoryInterface {
+
+    override fun observeChatsForUser(userId: String): Flow<List<Chat>> {
+        // TODO: Firestore whereArrayContains("participantIds", userId)
+        return flowOf(emptyList())
+    }
 
     override fun observeChats(): Flow<List<Chat>> {
         // TODO: Firestore collection listener
